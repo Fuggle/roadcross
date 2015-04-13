75cf05e83cfa240481edb90cee2e198431cd85d5
Fuggle <whoiscatnow@gmail.com>
2015-04-14 08:48:38 +1000

    updates to the initial arduino code

--- roadcross/roadcross.ino
+++ roadcross/roadcross.ino
@@ -2,18 +2,28 @@ int playerBlue = 5;
 int playerRed = 5;
 
 int playerOneButton = 13; //pin no for player button
-int playerTwoButton = 14; //pin no for player button
-int ledPin = 15; //pin no for LED output
+int playerTwoButton = 12; //pin no for player button
+int ledPin1 = 8; //pin no for LED output
+int ledPin2 = 9;
+int ledPin3 = 10;
 
 int playerOneVal = 0; //to be the value of the input button
 int playerTwoVal = 0; //to be the value of the input button
 
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
-  
+  Serial.println(playerOneButton);
   pinMode(playerOneButton, INPUT);
   pinMode(playerTwoButton, INPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  
+  //turn them all off to begin with
+  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin2, LOW);
+  digitalWrite(ledPin3, LOW);
 }
 
 void loop() {
@@ -23,16 +33,33 @@ void loop() {
   
   //check the input and count a press if pressed.
   //pass to keypress function.
-  
-  
+  Serial.println(playerOneVal);
+  Serial.println(playerTwoVal);
+ 
   //drawObjects code from lua example will become output to LEDs.
-    
+  if (playerOneVal != 0) {
+    digitalWrite(ledPin3, HIGH);
+  } else {
+    digitalWrite(ledPin3, LOW);
+  }
+  /*if (playerTwoVal != 0) {
+    digitalWrite(ledPin2, HIGH);
+  } else {
+    digitalWrite(ledPin2, LOW);
+  }
+  
+  */
+ 
+  
+  
 }
 
-boolean checkScore(player) {
+//checks that the player score does not exceed boundaries, and the sum of both is 10
+//returns true or false if the score is able to be incremented
+/*boolean checkScore(player) {
   if (playerBlue + playerRed == 10 && player >= 0 && player < 10 ) {
     return true;
   } else { 
     return false;
   }
-}
+}*/
